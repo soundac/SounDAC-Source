OneGramTech/onegram-core

Testing for invalid block ids

tests/tests/block_tests.cpp
@@ -255,59 +255,96 @@ BOOST_AUTO_TEST_CASE( fork_blocks )
          } FC_CAPTURE_AND_RETHROW( ("db2") );
       }
 
-      BOOST_TEST_MESSAGE( "Adding blocks 11 through 13 to db1 only" );
-      for( uint32_t i = 11; i <= 13; ++i )
+      for( uint32_t j = 0; j <= 4; j += 4 )
       {
-         auto b = db1.generate_block(db1.get_slot_time(1), db1.get_scheduled_witness(1), init_account_priv_key, database::skip_nothing);
-      }
-      string db1_tip = db1.head_block_id().str();
+         // add blocks 11 through 13 to db1 only
+         BOOST_TEST_MESSAGE( "Adding 3 blocks to db1 only" );
+         for( uint32_t i = 11 + j; i <= 13 + j; ++i )
+         {
+            BOOST_TEST_MESSAGE( i );
+            auto b = db1.generate_block(db1.get_slot_time(1), db1.get_scheduled_witness(1), init_account_priv_key, database::skip_nothing);
+         }
+         string db1_tip = db1.head_block_id().str();
 
-      // add blocks 14 and 15 to db2 only, leaving space for 11 through 13
-      BOOST_TEST_MESSAGE( "Add blocks 14 through 16 to db2 only" );
-      uint32_t next_slot = 3;
-      for( uint32_t i = 14; i <= 16; ++i )
-      {
-         auto b =  db2.generate_block(db2.get_slot_time(next_slot), db2.get_scheduled_witness(next_slot), init_account_priv_key, database::skip_nothing);
-         next_slot = 1;
-         // notify both databases of the new block.
-         // only db2 should switch to the new fork, db1 should not
-         PUSH_BLOCK( db1, b );
+         // add different blocks 11 through 13 to db2 only
+         BOOST_TEST_MESSAGE( "Add 3 different blocks to db2 only" );
+         uint32_t next_slot = 3;
+         for( uint32_t i = 11 + j; i <= 13 + j; ++i )
+         {
+            BOOST_TEST_MESSAGE( i );
+            auto b =  db2.generate_block(db2.get_slot_time(next_slot), db2.get_scheduled_witness(next_slot), init_account_priv_key, database::skip_nothing);
+            next_slot = 1;
+            // notify both databases of the new block.
+            // only db2 should switch to the new fork, db1 should not
+            PUSH_BLOCK( db1, b );
+            BOOST_CHECK_EQUAL(db1.head_block_id().str(), db1_tip);
+            BOOST_CHECK_EQUAL(db2.head_block_id().str(), b.id().str());
+         }
+
+         //The two databases are on distinct forks now, but at the same height.
+         BOOST_CHECK_EQUAL(db1.head_block_num(), 13u + j);
+         BOOST_CHECK_EQUAL(db2.head_block_num(), 13u + j);
+         BOOST_CHECK( db1.head_block_id() != db2.head_block_id() );
+
+         //Make a block on db2, make it invalid, then
+         //pass it to db1 and assert that db1 doesn't switch to the new fork.
+         signed_block good_block;
+         {
+            auto b = db2.generate_block(db2.get_slot_time(1), db2.get_scheduled_witness(1), init_account_priv_key, database::skip_nothing);
+            good_block = b;
+            b.transactions.emplace_back(signed_transaction());
+            b.transactions.back().operations.emplace_back(transfer_operation());
+            b.sign( init_account_priv_key );
+            BOOST_CHECK_EQUAL(b.block_num(), 14u + j);
+            GRAPHENE_CHECK_THROW(PUSH_BLOCK( db1, b ), fc::exception);
+
+            // At this point, `fetch_block_by_number` will fetch block from fork_db,
+            //    so unable to reproduce the issue which is fixed in PR #938
+            //    https://github.com/bitshares/bitshares-core/pull/938
+            fc::optional<signed_block> previous_block = db1.fetch_block_by_number(1);
+            BOOST_CHECK ( previous_block.valid() );
+            uint32_t db1_blocks = db1.head_block_num();
+            for( uint32_t curr_block_num = 2; curr_block_num <= db1_blocks; ++curr_block_num )
+            {
+               fc::optional<signed_block> curr_block = db1.fetch_block_by_number( curr_block_num );
+               BOOST_CHECK( curr_block.valid() );
+               BOOST_CHECK_EQUAL( curr_block->previous.str(), previous_block->id().str() );
+               previous_block = curr_block;
+            }
+         }
+         BOOST_CHECK_EQUAL(db1.head_block_num(), 13u + j);
          BOOST_CHECK_EQUAL(db1.head_block_id().str(), db1_tip);
-         BOOST_CHECK_EQUAL(db2.head_block_id().str(), b.id().str());
+
+         if( j == 0 )
+         {
+            // assert that db1 switches to new fork with good block
+            BOOST_CHECK_EQUAL(db2.head_block_num(), 14u + j);
+            PUSH_BLOCK( db1, good_block );
+            BOOST_CHECK_EQUAL(db1.head_block_id().str(), db2.head_block_id().str());
+         }
       }
 
-      //The two databases are on distinct forks now, but at the same height.
-      BOOST_CHECK_EQUAL(db1.head_block_num(), 13u);
-      BOOST_CHECK_EQUAL(db2.head_block_num(), 13u);
-      //Make a block on db2, make it invalid, then
-      //pass it to db1 and assert that db1 doesn't switch to the new fork.
-      signed_block good_block;
+      // generate more blocks to push the forked blocks out of fork_db
+      BOOST_TEST_MESSAGE( "Adding more blocks to db1, push the forked blocks out of fork_db" );
+      for( uint32_t i = 1; i <= 50; ++i )
       {
-         auto b = db2.generate_block(db2.get_slot_time(1), db2.get_scheduled_witness(1), init_account_priv_key, database::skip_nothing);
-         good_block = b;
-         b.transactions.emplace_back(signed_transaction());
-         b.transactions.back().operations.emplace_back(transfer_operation());
-         b.sign( init_account_priv_key );
-         BOOST_CHECK_EQUAL(b.block_num(), 14u);
-         GRAPHENE_CHECK_THROW(PUSH_BLOCK( db1, b ), fc::exception);
-         // Issue #938 make sure db is in a good state
+         db1.generate_block(db1.get_slot_time(1), db1.get_scheduled_witness(1), init_account_priv_key, database::skip_nothing);
+      }
+
+      {
+         // PR #938 make sure db is in a good state https://github.com/bitshares/bitshares-core/pull/938
+         BOOST_TEST_MESSAGE( "Checking whether all blocks on disk are good" );
          fc::optional<signed_block> previous_block = db1.fetch_block_by_number(1);
          BOOST_CHECK ( previous_block.valid() );
-         for( uint32_t curr_block_num = 2; curr_block_num <= 13; ++curr_block_num )
+         uint32_t db1_blocks = db1.head_block_num();
+         for( uint32_t curr_block_num = 2; curr_block_num <= db1_blocks; ++curr_block_num )
          {
             fc::optional<signed_block> curr_block = db1.fetch_block_by_number( curr_block_num );
             BOOST_CHECK( curr_block.valid() );
-            BOOST_CHECK_EQUAL( curr_block->previous, previous_block->id() );
+            BOOST_CHECK_EQUAL( curr_block->previous.str(), previous_block->id().str() );
             previous_block = curr_block;
          }
       }
-      BOOST_CHECK_EQUAL(db1.head_block_num(), 13u);
-      BOOST_CHECK_EQUAL(db1.head_block_id().str(), db1_tip);
-
-      // assert that db1 switches to new fork with good block
-      BOOST_CHECK_EQUAL(db2.head_block_num(), 14u);
-      PUSH_BLOCK( db1, good_block );
-      BOOST_CHECK_EQUAL(db1.head_block_id().str(), db2.head_block_id().str());
    } catch (fc::exception& e) {
       edump((e.to_detail_string()));
       throw;

soundac/SounDAC-Source

tests/tests/block_tests.cpp
@@ -264,59 +264,96 @@ BOOST_AUTO_TEST_CASE( fork_blocks )
          } FC_CAPTURE_AND_RETHROW( ("db2") );
       }
 
-      BOOST_TEST_MESSAGE( "Adding blocks 11 through 13 to db1 only" );
-      for( uint32_t i = 11; i <= 13; ++i )
+      for( uint32_t j = 0; j <= 4; j += 4 )
       {
-         auto b = db1.generate_block(db1.get_slot_time(1), db1.get_scheduled_witness(1), init_account_priv_key(), database::skip_nothing);
-      }
-      string db1_tip = db1.head_block_id().str();
+         // add blocks 11 through 13 to db1 only
+         BOOST_TEST_MESSAGE( "Adding 3 blocks to db1 only" );
+         for( uint32_t i = 11 + j; i <= 13 + j; ++i )
+         {
+            BOOST_TEST_MESSAGE( i );
+            auto b = db1.generate_block(db1.get_slot_time(1), db1.get_scheduled_witness(1), init_account_priv_key(), database::skip_nothing);
+         }
+         string db1_tip = db1.head_block_id().str();
 
-      // add blocks 14 and 15 to db2 only, leaving space for 11 through 13
-      BOOST_TEST_MESSAGE( "Add blocks 14 through 16 to db2 only" );
-      uint32_t next_slot = 3;
-      for( uint32_t i = 14; i <= 16; ++i )
-      {
-         auto b =  db2.generate_block(db2.get_slot_time(next_slot), db2.get_scheduled_witness(next_slot), init_account_priv_key(), database::skip_nothing);
-         next_slot = 1;
-         // notify both databases of the new block.
-         // only db2 should switch to the new fork, db1 should not
-         PUSH_BLOCK( db1, b );
+         // add different blocks 11 through 13 to db2 only
+         BOOST_TEST_MESSAGE( "Add 3 different blocks to db2 only" );
+         uint32_t next_slot = 3;
+         for( uint32_t i = 11 + j; i <= 13 + j; ++i )
+         {
+            BOOST_TEST_MESSAGE( i );
+            auto b =  db2.generate_block(db2.get_slot_time(next_slot), db2.get_scheduled_witness(next_slot), init_account_priv_key(), database::skip_nothing);
+            next_slot = 1;
+            // notify both databases of the new block.
+            // only db2 should switch to the new fork, db1 should not
+            PUSH_BLOCK( db1, b );
+            BOOST_CHECK_EQUAL(db1.head_block_id().str(), db1_tip);
+            BOOST_CHECK_EQUAL(db2.head_block_id().str(), b.id().str());
+         }
+
+         //The two databases are on distinct forks now, but at the same height.
+         BOOST_CHECK_EQUAL(db1.head_block_num(), 13u + j);
+         BOOST_CHECK_EQUAL(db2.head_block_num(), 13u + j);
+         BOOST_CHECK( db1.head_block_id() != db2.head_block_id() );
+
+         //Make a block on db2, make it invalid, then
+         //pass it to db1 and assert that db1 doesn't switch to the new fork.
+         signed_block good_block;
+         {
+            auto b = db2.generate_block(db2.get_slot_time(1), db2.get_scheduled_witness(1), init_account_priv_key(), database::skip_nothing);
+            good_block = b;
+            b.transactions.emplace_back(signed_transaction());
+            b.transactions.back().operations.emplace_back(transfer_operation());
+            b.sign( init_account_priv_key() );
+            BOOST_CHECK_EQUAL(b.block_num(), 14u + j);
+            MUSE_CHECK_THROW(PUSH_BLOCK( db1, b ), fc::exception);
+
+            // At this point, `fetch_block_by_number` will fetch block from fork_db,
+            //    so unable to reproduce the issue which is fixed in PR #938
+            //    https://github.com/bitshares/bitshares-core/pull/938
+            fc::optional<signed_block> previous_block = db1.fetch_block_by_number(1);
+            BOOST_CHECK ( previous_block.valid() );
+            uint32_t db1_blocks = db1.head_block_num();
+            for( uint32_t curr_block_num = 2; curr_block_num <= db1_blocks; ++curr_block_num )
+            {
+               fc::optional<signed_block> curr_block = db1.fetch_block_by_number( curr_block_num );
+               BOOST_CHECK( curr_block.valid() );
+               BOOST_CHECK_EQUAL( curr_block->previous.str(), previous_block->id().str() );
+               previous_block = curr_block;
+            }
+         }
+         BOOST_CHECK_EQUAL(db1.head_block_num(), 13u + j);
          BOOST_CHECK_EQUAL(db1.head_block_id().str(), db1_tip);
-         BOOST_CHECK_EQUAL(db2.head_block_id().str(), b.id().str());
+
+         if( j == 0 )
+         {
+            // assert that db1 switches to new fork with good block
+            BOOST_CHECK_EQUAL(db2.head_block_num(), 14u + j);
+            PUSH_BLOCK( db1, good_block );
+            BOOST_CHECK_EQUAL(db1.head_block_id().str(), db2.head_block_id().str());
+         }
       }
 
-      //The two databases are on distinct forks now, but at the same height.
-      BOOST_CHECK_EQUAL(db1.head_block_num(), 13u);
-      BOOST_CHECK_EQUAL(db2.head_block_num(), 13u);
-      //Make a block on db2, make it invalid, then
-      //pass it to db1 and assert that db1 doesn't switch to the new fork.
-      signed_block good_block;
+      // generate more blocks to push the forked blocks out of fork_db
+      BOOST_TEST_MESSAGE( "Adding more blocks to db1, push the forked blocks out of fork_db" );
+      for( uint32_t i = 1; i <= 50; ++i )
       {
-         auto b = db2.generate_block(db2.get_slot_time(1), db2.get_scheduled_witness(1), init_account_priv_key(), database::skip_nothing);
-         good_block = b;
-         b.transactions.emplace_back(signed_transaction());
-         b.transactions.back().operations.emplace_back(transfer_operation());
-         b.sign( init_account_priv_key() );
-         BOOST_CHECK_EQUAL(b.block_num(), 14u);
-         MUSE_CHECK_THROW(PUSH_BLOCK( db1, b ), fc::exception);
-         // Issue #938 make sure db is in a good state
+         db1.generate_block(db1.get_slot_time(1), db1.get_scheduled_witness(1), init_account_priv_key(), database::skip_nothing);
+      }
+
+      {
+         // PR #938 make sure db is in a good state https://github.com/bitshares/bitshares-core/pull/938
+         BOOST_TEST_MESSAGE( "Checking whether all blocks on disk are good" );
          fc::optional<signed_block> previous_block = db1.fetch_block_by_number(1);
          BOOST_CHECK ( previous_block.valid() );
-         for( uint32_t curr_block_num = 2; curr_block_num <= 13; ++curr_block_num )
+         uint32_t db1_blocks = db1.head_block_num();
+         for( uint32_t curr_block_num = 2; curr_block_num <= db1_blocks; ++curr_block_num )
          {
             fc::optional<signed_block> curr_block = db1.fetch_block_by_number( curr_block_num );
             BOOST_CHECK( curr_block.valid() );
-            BOOST_CHECK_EQUAL( curr_block->previous, previous_block->id() );
+            BOOST_CHECK_EQUAL( curr_block->previous.str(), previous_block->id().str() );
             previous_block = curr_block;
          }
       }
-      BOOST_CHECK_EQUAL(db1.head_block_num(), 13u);
-      BOOST_CHECK_EQUAL(db1.head_block_id().str(), db1_tip);
-
-      // assert that db1 switches to new fork with good block
-      BOOST_CHECK_EQUAL(db2.head_block_num(), 14u);
-      PUSH_BLOCK( db1, good_block );
-      BOOST_CHECK_EQUAL(db1.head_block_id().str(), db2.head_block_id().str());
    } catch (fc::exception& e) {
       edump((e.to_detail_string()));
       throw;